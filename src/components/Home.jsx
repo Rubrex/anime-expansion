b4c1cec1930e8b9835bb28cf48c4ce52bcd60e20
Rubel Hossain <rubrex96@hotmail.com>
2022-10-09 08:43:56 +0600

    Basic Footer Added from Kitwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  return <div className="container mx-auto"></div>;
 };
 
 export default Home;
